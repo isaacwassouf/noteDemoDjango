7a7a7c05f0b3832aba63c28cf34d8ffe2ee6a4a8
isaacwassouf <izakwassouf@gmail.com>
2018-08-19 19:04:32 +0300

    moved to the default user model

--- posts/models.py
+++ posts/models.py
@@ -1,25 +1,17 @@
 from django.db import models
 from datetime import datetime
+from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-
-class User(models.Model):
-    firstname=models.CharField(max_length=20)
-    lastname=models.CharField(max_length=20)
-    age=models.IntegerField()
-    username=models.CharField(max_length=30)
-    password=models.CharField(max_length=30)
-
-    def __str__(self):
-        return "{} {}".format(self.firstname,self.lastname)
 
 
 class Post(models.Model):
     title=models.CharField(max_length=200)
     content=models.TextField()
     createted_at= models.DateField(default=datetime.now,blank=True)
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    user= models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE,default=1)
 
     def __str__(self):
         return self.title
+    
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
-from .models import Post,User
+from django.http import HttpResponse,Http404
+from .models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
 
@@ -8,66 +10,78 @@ def index(request):
     if(request.method=='POST'):
         username=request.POST['username']
         password=request.POST['password']
-        user=User.objects.filter(username=username,password=password)
-        if (user.exists()):
-            user =User.objects.get(username=username)
-            id=user.id
-            return redirect('/posts/'+ str(id))
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/posts/'+str(user.id))
 
 
 
 def posts(request,uid):
-    posts=Post.objects.filter(user=uid)
-    context={
-        'posts':posts,
-        'uid': uid
-    }
-    return render(request,'posts/index.html',context)
+    if request.user.is_authenticated:
+        posts=Post.objects.filter(user=uid)
+        context={
+            'posts':posts,
+            'uid': uid,
+        }
+        return render(request,'posts/index.html',context)
+    else:
+        return HttpResponse("User is not looged in")
 
 
 def item(request,id,uid):
-    post= Post.objects.get(id=id,user=uid)
-    context={
-        'post': post
-    }
-    return render(request,'posts/item.html',context)
+    if request.user.is_authenticated:
+        post= Post.objects.get(id=id,user=uid)
+        context={
+            'post': post
+        }
+        print(request.user.id)
+        return render(request,'posts/item.html',context)
+    else:
+        return HttpResponse("User is not logged in")
 
 def modify(request,id,uid):
-    if (request.method=='GET'):
-        post=Post.objects.get(id=id,user=uid)
-        context={
-        'post':post
-        }
-        return render(request,'posts/modify.html',context)
+    if request.user.is_authenticated:
+        if (request.method=='GET'):
+            post=Post.objects.get(id=id,user=uid)
+            context={
+            'post':post
+            }
+            return render(request,'posts/modify.html',context)
 
+        else:
+            title=request.POST['title']
+            content=request.POST['content']
+
+            post=Post.objects.get(id=id)
+            post.title=title
+            post.content=content
+            post.save()
+            return redirect('/posts/'+str(uid))
     else:
-        title=request.POST['title']
-        content=request.POST['content']
-
-        post=Post.objects.get(id=id)
-        post.title=title
-        post.content=content
-        post.save()
-        return redirect('/posts/'+str(uid))
+        return HttpResponse("User is not logged in")
 
 
 def add(request,uid):
-    if(request.method =='GET'):
-        title=request.GET['title']
-        context={
-            'title':title,
-            'uid':uid
-        }
-        return render(request,'posts/add.html',context)
+    if request.user.is_authenticated:
+        if(request.method =='GET'):
+            title=request.GET['title']
+            context={
+                'title':title,
+                'uid':uid
+            }
+            return render(request,'posts/add.html',context)
 
+        else:
+            title=request.POST['title']
+            content=request.POST['content']
+
+            user= User.objects.get(id=uid)
+            newPost=Post( title=title,content=content,user=user)
+            newPost.save()
+            return redirect('/posts/'+str(uid))
     else:
-        title=request.POST['title']
-        content=request.POST['content']
-
-        user= User.objects.get(id=uid)
-        newPost=Post( title=title,content=content,user=user)
-        newPost.save()
-        return redirect('/posts/'+str(uid))
+        return HttpResponse("User is not looged in")
 
 def home(request):
     if(request.method=='GET'):
@@ -81,11 +95,10 @@ def signup(request):
     elif (request.method=='POST'):
         firstname=request.POST['firstname']
         lastname=request.POST['lastname']
-        age=request.POST['age']
         username=request.POST['username']
         password= request.POST['password']
 
-        newUser=User(firstname=firstname,lastname=lastname,age=age,username=username,password=password)
+        newUser=User.objects.create_user(username=username,password=password,first_name=firstname,last_name=lastname)
         newUser.save()
-        return redirect('/posts/'+str(newUser.id))
+        return redirect('/')
         
